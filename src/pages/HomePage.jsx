3f629f4fea9896808ab0c3d8f385bee5ad951f4f
KrishJain <krishjain1903@gmail.com>
2025-04-21 00:17:14 +0530

    Made Components based on Routes and Linked them with NavLink Component

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
+function HomePage() {
+  return (
+    <div>
+      <PageNav />
+      Home Page
+    </div>
+  );
+}
+
+export default HomePage;
