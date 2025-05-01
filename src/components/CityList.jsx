f40e534021accc061d8c97c3ed30634981bf70fb
KrishJain <krishjain1903@gmail.com>
2025-05-01 19:21:48 +0530

    Made a Fake Cities API and Added Citi List Component

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,22 @@
 import styles from "../CityList.module.css";
+import Spinner from "../Spinner";
+import CityItem from "./CityItem";
+import Message from "../Message";
 
-function CityList() {
-  return <ul className={styles.CityList}>List of cities</ul>;
+function CityList({ cities, isLoading }) {
+  if (isLoading) {
+    return <Spinner />;
+  } else if (!cities.length) {
+    return <Message message="No Cities Found" />;
+  } else {
+    return (
+      <ul className={styles.cityList}>
+        {cities.map((city) => {
+          return <CityItem city={city} key={city.id} />;
+        })}
+      </ul>
+    );
+  }
 }
 
 export default CityList;
