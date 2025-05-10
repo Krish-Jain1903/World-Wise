0473dded1b5c55c58b183ae9e0f894f63ee0d313
KrishJain <krishjain1903@gmail.com>
2025-05-11 01:47:37 +0530

    Added Map using Third Party Library Leaflet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,50 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import styles from "../Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const lat = searchParams.get("lat");
+  // const lng = searchParams.get("lng");
 
-  const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [mapPosition, setMapPosition] = useState([19.07283, 72.88261]);
+  const { cities } = useCities();
+
+  // const navigate = useNavigate();
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position Lat :- {lat} and Lng :- {lng}
-      </h1>
-      <button onClick={() => setSearchParams({ lat: 100, lng: 100 })}>
-        Change Position
-      </button>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={false}
+        className={styles.map}
+      >
+        {/** This TileLayer will Have Different Style of Map Because of URL Change */}
+
+        {/* <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        /> */}
+
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => {
+          return (
+            <Marker
+              position={[city.position.lat, city.position.lng]}
+              key={city.id}
+            >
+              <Popup>{city.cityName}</Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
     </div>
   );
 }
